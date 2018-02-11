e8a76e8eb3dc60308890761fcef83ec7ed2d70d6
Abhishek Kumar <abhishekint16@gmail.com>
2018-02-11 15:27:56 +0530

    extending RelativeLayout and implemented all constructor required

--- imagegrad/src/main/java/flikster/com/imagegrad/ImageGrad.java
+++ imagegrad/src/main/java/flikster/com/imagegrad/ImageGrad.java
@@ -1,8 +1,27 @@
 package flikster.com.imagegrad;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
 /**
  * Created by abhishek on 11-02-2018.
  */
 
-public class ImageGrad {
+public class ImageGrad extends RelativeLayout{
+    public ImageGrad(Context context) {
+        super(context);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
