884a0e308a254916e2232a7cf3110fcfa238a60d
Abhishek Kumar <abhishekint16@gmail.com>
2018-02-11 15:35:59 +0530

    declaring core items and alter constructors to receive attributes and styles

--- imagegrad/src/main/java/flikster/com/imagegrad/ImageGrad.java
+++ imagegrad/src/main/java/flikster/com/imagegrad/ImageGrad.java
@@ -1,7 +1,10 @@
 package flikster.com.imagegrad;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 /**
@@ -9,19 +12,34 @@ import android.widget.RelativeLayout;
  */
 
 public class ImageGrad extends RelativeLayout{
+
+    /** Core Items*/
+    private Context mContext;
+    private AttributeSet attrs;
+    private int styleAttr;
+    private View view;
+
     public ImageGrad(Context context) {
         super(context);
+        this.mContext=context;
     }
 
     public ImageGrad(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.mContext=context;
+        this.attrs=attrs;
     }
 
     public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.mContext=context;
+        this.attrs=attrs;
+        this.styleAttr=defStyleAttr;
     }
 
+    /*@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-    }
+
+    }*/
 }
